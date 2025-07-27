6155a162cca9e9b5d9d04b39fe0868c9a7fcf051
Jacob <jmcentir@gmail.com>
2025-07-27 18:49:49 -0400

    Added basic Player/Match functionality with testing

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from abc import ABC, abstractmethod
+import random
 
+##################################################################
+########################## CONSTANTS #############################
+##################################################################
+
+NUM_COLUMNS = 3
+NUM_PLAYERS = 2
+
+##################################################################
+######################## GAME OBJECTS ############################
+##################################################################
 
 class PlayerBoard:
     def __init__(self, board: list[list[int]] = None) -> None:
         # We allow for a premade board, mostly for testing
-        self.board = board or [[0] * 3] * 3
+        self.board = board or [[] for i in range(NUM_COLUMNS)]
 
-    def get_row_score(self, i: int) -> int:
+    def col_score(self, i: int) -> int:
         total_score = 0
-        #roll_ct = [self.board[i].count(num) for num in range(1,7)]
         for die_roll in range(1,7):
             roll_ct = self.board[i].count(die_roll)
             # Score contributed by a die roll is the sum of those rolls * the number of rolls
@@ -17,15 +28,73 @@ class PlayerBoard:
 
         return total_score
     
-    def get_total_score(self) -> int:
+    def total_score(self) -> int:
         total_score = 0
-        for row in range(3):
-            total_score += self.get_row_score(row)
+        for col in range(NUM_COLUMNS):
+            total_score += self.col_score(col)
 
         return total_score
+    
+    # Modifies the col in place to add the rolled die
+    # This is definitely way too much abstraction lol
+    def add_die_to_col(self, col_num: int, die_roll: int):
+        self.board[col_num].append(die_roll)
 
 
-a = PlayerBoard()
+class Strategy(ABC):
+    @abstractmethod
+    # Takes in a board state and die roll and returns the column in which to place the die
+    # This is where all of the actual work happens
+    # Accepts board_state as [my_board, opponent_board]
+    # Will not return a col number for a full col
+    # TODO: handle game end here
+    def get_placement_column(self, board_state: list[PlayerBoard], die_roll: int) -> int:
+        pass
 
-print(a.board)
-print(a.get_total_score())
+
+class Player:
+    def __init__(self, strategy: Strategy):
+        self.strategy = strategy
+
+    # Accepts board_state as [my_board, opponent_board]
+    def take_turn(self, board_state: list[PlayerBoard]):
+        die_roll = random.randint(1,6)
+        placement_col = self.strategy.get_placement_column(board_state, die_roll)
+        board_state[0].add_die_to_col(placement_col, die_roll)
+
+
+# NOTE: handles game board, which could be broken out into it's own class
+class Match:
+    def __init__(self, players: list[Player]):
+        self.players = players
+        # for player_num in range(0, NUM_PLAYERS):
+        #     self.players[player_num].player_num = player_num
+
+        # NOTE: We can't really do this with more than 2
+        self.game_board = [PlayerBoard()] * NUM_PLAYERS
+
+    # Takes in a player number and returns the board "from their perspective"
+    def _get_board_state_for_player(self, player_num: int) -> list[PlayerBoard]:
+        return [
+            self.game_board[player_num],
+            self.game_board[player_num - 1]
+        ]
+    
+    def do_rounds(self, num_rounds: int):
+        pass
+
+##################################################################
+######################### STRATEGIES ############################
+##################################################################
+
+# Randomly determines where to place die
+class RandomStrat(Strategy):
+    def get_placement_column(self, board_state: list[PlayerBoard], die_roll: int) -> int:
+        open_col_nums = [i for i in range(0, NUM_COLUMNS) if len(board_state[i]) < 2]
+        # TODO don't return full col
+        return random.choice(open_col_nums)
+
+##################################################################
+########################### MATCHES ##############################
+##################################################################
+
--- test_main.py
+++ test_main.py
@@ -1,5 +1,16 @@
+from unittest.mock import MagicMock, patch
+import random
 import pytest
-from main import PlayerBoard
+
+from main import (
+    Player,
+    PlayerBoard,
+    Strategy,
+    NUM_COLUMNS
+)
+
+EMPTY_BOARD = [[] for i in range(NUM_COLUMNS)]
+
 
 def test_board_scoring():
     test_board = PlayerBoard([
@@ -10,7 +21,38 @@ def test_board_scoring():
 
     expected_score = [6,1,22]
 
-    for i in range(3):
-        assert test_board.get_row_score(i) == expected_score[i]
+    for i in range(NUM_COLUMNS):
+        assert test_board.col_score(i) == expected_score[i]
 
-    assert test_board.get_total_score() == 29
+    assert test_board.total_score() == sum(expected_score)
+
+def test_empty_board_scoring():
+    test_board = PlayerBoard()
+
+    assert test_board.board == EMPTY_BOARD
+    assert test_board.total_score() == 0
+
+def test_multiples_board_scoring():
+    test_board = PlayerBoard([
+        [1,1,3], # 2 * (1 + 1) + 3 = 7
+        [6,6,6], # 3 * (6 + 6 + 6) = 54
+        [5,5,3] # 2 * (5 + 5) + 3 = 23
+    ])
+
+    expected_score = [7,54,23]
+
+    for i in range(NUM_COLUMNS):
+        assert test_board.col_score(i) == expected_score[i]
+
+    assert test_board.total_score() == sum(expected_score)
+
+def test_player_can_modify_board():
+    with patch('random.randint', return_value=5):
+        test_board = [PlayerBoard(), PlayerBoard()]
+        mock_strategy = MagicMock()
+        mock_strategy.get_placement_column.return_value = 1
+
+        test_player = Player(mock_strategy)
+        test_player.take_turn(test_board)
+        assert test_board[0].board == [[],[5],[]]
+        assert test_board[1].board == EMPTY_BOARD
